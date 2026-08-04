1e58cdb7335d53cf1e7825dc763f7050036108d6
mariaeduarda-nog <maria.e.nogueira6@edu.senai.br>
2026-08-04 14:00:13 -0300

    laços de repetição

--- Aula-03/Revisao/Reepeticao/Program.cs
+++ Aula-03/Revisao/Reepeticao/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+           sintaxe:
+            enquanta uma condição for verdadeira  execute um conjunto de comeandos
+
+            for -> quando sabemos a quantidade de repetições.
+            while -> quando a repetição depende de uma condição.
+            do ... while -> quando precisa executar ao menos uma vez o bloco de codigo
+        */
+        /* laço for: imprimir numeros de 1 ate o 10 */
+
+    Console.WriteLine("imprimir numeros de 1 ate 10, utilizando o laço for.");
+    for (int numeros = 1; numeros <= 10; numeros++)
+        {
+              Console.WriteLine(numeros);
+        }
+
+ /* laço for: contagem regressiva */
+
+    Console.WriteLine("imprimir contagem regressiva.");
+    for (int numeros = 1; numeros >= 10; numeros--)
+        {
+              Console.WriteLine(numeros);
+        }
+    }
+}
