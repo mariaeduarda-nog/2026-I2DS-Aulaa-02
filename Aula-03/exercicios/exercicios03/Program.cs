767c47e94abab4a50382effdde822a5bfb797144
mariaeduarda-nog <maria.e.nogueira6@edu.senai.br>
2026-08-05 09:38:26 -0300

    exercicios

--- Aula-03/exercicios/exercicios03/Program.cs
+++ Aula-03/exercicios/exercicios03/Program.cs
@@ -1,19 +1,38 @@
-﻿/*
-Criar um sistema que calcule a média de um aluno e informe sua situação.
-
-Conceitos utilizados:
-
-- Variáveis;
-- Condições;
-- Operadores relacionais;
-- Métodos. */
-
-/* EXERCICIO 03 */
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        double nota1;
+        double nota2;
+        double nota3;
+        double media;
+        string nome;
+
+        Console.WriteLine("Digite seu nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Acesso Permitido! ");
+        
+        Console.WriteLine("Agora, digite sua Nota 1: ");
+        nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Agora, digite sua Nota 2: ");
+        nota2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Agora, digite sua Nota 3: ");
+        nota3 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Calculando... ");
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+
+        if (media >= 7)
+        {
+            Console.WriteLine(nome + ", você foi Aprovado, Média: " + media);
+        }
+        else
+        {
+            Console.WriteLine(nome + ", você foi Reprovado, Média: " + media);
+        }
     }
 }
