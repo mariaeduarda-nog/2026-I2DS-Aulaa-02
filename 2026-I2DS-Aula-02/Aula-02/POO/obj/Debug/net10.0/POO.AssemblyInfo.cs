41c34d66087b5de410ac2a73492557ca30e548ef
mariaeduarda-nog <maria.e.nogueira6@edu.senai.br>
2026-08-11 10:29:35 -0300

    exercicios

--- 2026-I2DS-Aula-02/Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ 2026-I2DS-Aula-02/Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c493101b6e9eed4bd7834fd944b3026e7231047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89fbe5d381d9de75847e9ce9d21dd15c4a26a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2026-I2DS-Aula-02/Aula-05/Exercicio-02/Aluno/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Primeiro aluno
+        Aluno aluno1 = new Aluno();
+        aluno1.Nome = "Matheus";
+        aluno1.Email =  "Matheuscastilho003@gmail.com";
+        aluno1.Nascimento = "03/09/2009";
+        aluno1.RM = 4477;
+
+        // Primeiro aluno
+        Aluno aluno2 = new Aluno();
+        aluno2.Nome = "Julio";
+        aluno2.Email =  "Julionagaita@gmail.com";
+        aluno2.Nascimento = "02/03/2010";
+        aluno2.RM = 9988;
+
+     // Primeiro aluno
+        Aluno aluno3 = new Aluno();
+        aluno3.Nome = "Alice";
+        aluno3.Email =  "Lice@gmail.com";
+        aluno3.Nascimento = "09/09/2000";
+        aluno3.RM = 5234;
+
+        // Exibindo os Livro na Tela
+        Console.WriteLine("==== LIVROS ====");
+        Console.WriteLine($"\nAluno 01: {aluno1.Nome} | Email: {aluno1.Email}. | Nascimento: {aluno1.Nascimento} | RM: {aluno1.RM}");
+        Console.WriteLine($"Aluno 02: {aluno2.Nome} | Email: {aluno2.Email} | Nascimento: {aluno2.Nascimento} | RM: {aluno2.RM}");
+        Console.WriteLine($"Aluno 03: {aluno3.Nome} | Email: {aluno3.Email} | Nascimento: {aluno3.Nascimento} | RM: {aluno3.RM}");
+    }
+
+    public class Aluno
+    {
+        public string Nome {get; set;}
+        public string Email {get; set;}
+        public string Nascimento {get; set;}
+        public int RM {get; set;}
+    }
+}
