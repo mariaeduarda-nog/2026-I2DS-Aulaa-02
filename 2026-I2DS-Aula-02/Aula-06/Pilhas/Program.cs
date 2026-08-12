e94db52ab7a04e3d4c6a08a35dc71fecfe99c929
mariaeduarda-nog <maria.e.nogueira6@edu.senai.br>
2026-08-12 11:14:01 -0300

    aula-06

---
+++ 2026-I2DS-Aula-02/Aula-06/Pilhas/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("=== TRABALHANDO COM PILHAS (FILO)");
+
+        // 1 Criar uma nova pilha vazia
+        Stack<string> pilhaLivros = new Stack<string>();
+
+        // 2 Adicionar elementos em uma pilha
+        pilhaLivros.Push("Dom Quixote");
+        pilhaLivros.Push("O morro dos ventos uiventes");
+        pilhaLivros.Push("O cortico");
+
+        // 3 Percorrer todos os elementos de uma pilha
+        Console.WriteLine();
+        foreach (var livro in pilhaLivros)
+        {
+            Console.WriteLine(livro);
+        }
+
+        // 4 - Remover um elemento da pilha
+        string livroRemovido = pilhaLivros.Pop();
+        Console.WriteLine();
+        Console.WriteLine($"O livro retirado foi: {livroRemovido}");
+
+        pilhaLivros.Push("Dom Casmurro");
+        Console.WriteLine();
+        foreach (var livro in pilhaLivros)
+        {
+            Console.WriteLine(livro);
+        }
+    }
+}
